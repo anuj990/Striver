b6417d39eec6c073a65c1005be1f7e20c42535c8
Anuj <gp712223@gmail.com>
2026-06-30 12:51:54 +0530

    Time: 3 ms (46.29%), Space: 9.6 MB (76.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,13 +4,18 @@ public:
         if(s.length()!=t.length()){
             return false;
         }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i =0;i<s.length();i++){
-            if(s[i]!=t[i]){
-                return false;
-            }
+        int freq[26] = {0};
+        for(int i =0;i<s.size();i++){
+            freq[s[i]-'a']++;
         }
-        return true;
+        for (int i = 0; i < s.length(); i++) {
+        freq[t[i] - 'a']--;
+    }
+    for(int i =0;i<26;i++){
+        if(freq[i]!=0){
+            return false;
+        }
+    }
+    return true;
     }
 };
