2da1bb47370e371671f5955449763d9c01e8edbe
Anuj <gp712223@gmail.com>
2026-07-01 14:40:03 +0530

    Time: 3 ms (75.53%), Space: 9.8 MB (99.33%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int> m;
-        m['I']=1;
-        m['V']=5;
-        m['X']=10;
-        m['L']=50;
-        m['C']=100;
-        m['D']=500;
-        m['M']=1000;
-
-        int sum =0;
-        for(int i=0;i<s.size()-1;i++){
-            if(m[s[i]]<m[s[i+1]]){
-                sum-=m[s[i]];
+        int sum = 0;
+        for (int i=0; i<s.length(); i++) {
+            switch (s[i]) {
+                case 'I':
+                if ((s[i+1] != 'V') && (s[i+1] != 'X')) {
+                    sum += 1;
+                } else {
+                    sum -=1;
+                }
+                break;
+                case 'V':
+                    sum += 5;
+                    break;
+                case 'X':
+                    if ((s[i+1] != 'L') && (s[i+1] != 'C')) {
+                    sum += 10;
+                } else {
+                    sum -=10;
+                }
+                break;
+                case 'L':
+                    sum += 50;
+                    break;
+                case 'C':
+                    if ((s[i+1] != 'D') && (s[i+1] != 'M')) {
+                    sum += 100;
+                } else {
+                    sum -=100;
+                }
+                break;
+                case 'D':
+                    sum += 500;
+                    break;
+                case 'M':
+                    sum += 1000;
+                    break;
             }
-            else{
-                sum+=m[s[i]];
-            }
-            
         }
-        sum+=m[s[s.size()-1]];
         return sum;
-
     }
 };
